9dec48629046350cff9847b5ecae21ef5574e012
Ryan Troy Lattibeaudiere <r_latti@hotmail.com>
2019-11-01 20:19:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,9 +2,9 @@ def find_element_index(array, value_to_find)
 array.find_index(value_to_find)
 end
 
-def find_max_value(array)
-  array.max_by(find_min_value)
-end
+def find_max_value(ararray.max_by(find_min_value)
+endray)
+  
 
 def find_min_value(array)
   # Add your solution here
